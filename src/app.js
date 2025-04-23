bc5f773473df535ab7dbf642efc74eba7292388b
Atharv sinha <atharvdav@gmail.com>
2025-04-23 23:17:25 +0530

    file manage and server intialization

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+
+app.use("/test",(req,res) => {
+  res.send('Request is running on server');
+})
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`successfully listening on server ${PORT}`);
