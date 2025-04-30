e4c5b08fd6a1e6d1fc4d28bf3cbe9e087e3a5924
Atharv sinha <atharvdav@gmail.com>
2025-04-30 23:15:21 +0530

    working on api post and get

--- src/app.js
+++ src/app.js
@@ -5,24 +5,76 @@ const User = require("./models/user.js");
 
 const app = express();
 
+app.use(express.json());
+
+
+
 app.post("/signup",async (req,res)=> {
-  
-  const user = new User({
-    name: "Atharv sinha",
-    age: 22,
-    emailId: "atharv@gmail.com",
-    password: "atharv",
-    gender: "Male"
-  });
+  const user = await User(req.body);
+
   try {
     await user.save();
-    res.send("User added successfuly")
+    res.send("User created successfully");
+  } catch(err){
+    res.send(400).send("An error occured" + err.message);
+  }
+});
+
+// singleuser
+app.get("/user",async (req,res)=>{
+ const userEmail = req.body.emailId;
+ 
+ try {
+  const users = await User.findOne({emailId:userEmail});
+
+  if(!users){
+    res.status(400).send("User didnt exist")
+  }
+  else{
+    res.send(users);
+  }
+  
+ }catch(err){
+  res.send(400).send("An error occured" + err.message);
+}
+});
+
+app.get("/feed",async(req,res) => {
+  
+  try{
+    const users = await User.find({});
+
+  if(users.length == 0) {
+    res.status(400).send("users are not found");
+  }
+  else{
+    res.send(users);
+  }
   }catch(err){
-    res.status(400).send("An error occured while saving" + err.message);
+    res.status(400).send("An error occurred"+ err.message);
+  }
+  
+});
+
+
+app.get("/getbyid/:id",async(req,res) => {
+  const userId = req.params.id;
+  try {
+    const users = await User.findById(userId);
+
+    if(!users) {
+      res.status(400).send("Something went wrong !");
+      console.log(users);
+      
+    }
+    else{
+      res.send(users);
+    }
+  }catch(err){
+    res.status(400).send("An error occurred")
+    console.log(err.message);    
   }
 })
-
-
 
 
 const PORT = 4000;
